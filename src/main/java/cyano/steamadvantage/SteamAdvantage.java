94ba4a09a77c1c0e67b33f0867f06a79ad2b9294
Cyanobacterium <github.com/cyanobacteruim>
2015-05-17 13:14:24 -0400

    fixed typo

--- src/main/java/cyano/steamadvantage/SteamAdvantage.java
+++ src/main/java/cyano/steamadvantage/SteamAdvantage.java
@@ -38,7 +38,7 @@ public class SteamAdvantage
     /** The display name for this mod */
     public static final String NAME = "Steam Advantage";
     /** The version of this mod, in the format major.minor.update */
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0.0";
     
     /**
      * Pre-initialization step. Used for initializing objects and reading the 
